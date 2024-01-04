4e0bfa60ba1c9b517ae6753a8c15548e39684f6e
rajachaymae <rajafamilly633@gmail.com>
2024-01-04 07:49:38 -0800

    cal

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add ,sub, mul, div
+
+if __name__ == " __main__ ":
+    b = 5
+    a = 10
+    print(a '+' b '=' add(a,b)
+    print(a '+' b '=' add(a,b)
+    print(a '+' b '=' add(a,b)
+    print(a '+' b '=' add(a,b)
